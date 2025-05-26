6e42bcf889e76e09c52b754a6ee6d26ee6068c7b
usama_forayaje <usamaforayaje03@gmail.com>
2025-05-27 01:19:31 +0600

    remove import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import path from "path";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
